982ad8a5c6d21f6c2142c6a42c9107386f9a1e53
Sounisa <119640964+sounisa@users.noreply.github.com>
2023-02-27 19:41:17 -0800

    dotenv

--- db.js
+++ db.js
@@ -1,15 +1,9 @@
 import pkg from "pg";
 const {Pool} = pkg;
-// import dotenv from 'dotenv'
-// dotenv.config();
 
 
 const client = new Pool({
-    user: "sounisaa",
-    password: "",
-    host: "localhost",
-    port: 5432, //default DB port
-    database: "Grocery_List"
+    connectionString: process.env.DATABASE_URL
 })
 
 export {client};
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 import {client} from './db.js'
 import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
